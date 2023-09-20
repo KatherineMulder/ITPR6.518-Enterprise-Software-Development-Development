2aa462f8dadbf035c82e92a38c88a5d36d2bdd3e
Katherine M <102567666+KatherineMulder@users.noreply.github.com>
2023-09-20 20:35:37 +1200

    database --- trying to fix the database name

--- databasesetup/setup.go
+++ databasesetup/setup.go
@@ -1,28 +1,148 @@
 package databasesetup
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
+    "context"
+    "fmt"
+    "log"
+    "time"
 
-	_ "github.com/lib/pq"
+    "github.com/jackc/pgx/v5"
 )
 
-func DatabaseSetup() (*sql.DB, error) {
-	conninfo := "user=postgres password='' dbname=EnterpriseNotes host=127.0.0.1 port=5432 sslmode=disable"
+const (
+    host     = "localhost"
+    port     = 5432
+    user     = "postgres"
+    password = ""
+    dbname   = "EnterpriseNotes"
+)
 
-	db, err := sql.Open("postgres", conninfo)
-	if err != nil {
-		log.Println(err)
-	}
+type Note struct {
+    ID        int
+    Title     string
+    Content   string
+    Timestamp time.Time
+    Status    string
+    UserID    int
+}
 
-	dbName := "EnterpriseNotes"
-	_, err = db.Exec("create database " + dbName)
-	if err != nil {
-		//handle the error
-		log.Println(err)
-	}
+func DatabaseSetup() (*pgx.Conn, error) {
+    // Construct the connection string
+    connString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+        host, port, user, password, dbname)
 
-	fmt.Println("Database Setup Complete")
-	return db, nil
+    // Establish the database connection
+    conn, err := pgx.Connect(context.Background(), connString)
+    if err != nil {
+        log.Fatal(err)
+        return nil, err
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+    defer cancel()
+
+    if err := conn.Ping(ctx); err != nil {
+        log.Fatal("Connection to specified database failed: ", err)
+        return nil, err
+    }
+    fmt.Println("Connected successfully")
+
+    if err := createTables(conn); err != nil {
+        log.Fatal("Failed to create tables: ", err)
+        return nil, err
+    }
+
+    return conn, nil
+}
+
+func createTables(conn *pgx.Conn) error {
+    notesTable := `
+    CREATE TABLE IF NOT EXISTS Notes (
+        ID serial PRIMARY KEY,
+        title VARCHAR(255),
+        content TEXT,
+        timestamp TIMESTAMP,
+        status VARCHAR(255),
+        userID INT
+    );
+    `
+
+    usersTable := `
+    CREATE TABLE IF NOT EXISTS Users (
+        userID serial PRIMARY KEY,
+        name VARCHAR(255),
+        email VARCHAR(255) UNIQUE,
+        password_hash VARCHAR(255)
+    );
+    `
+
+    sharingTable := `
+    CREATE TABLE IF NOT EXISTS Sharing (
+        userID INT,
+        noteID INT,
+        status VARCHAR(255),
+        timestamp TIMESTAMP
+    );
+    `
+
+    _, err := conn.Exec(context.Background(), notesTable)
+    if err != nil {
+        return err
+    }
+
+    _, err = conn.Exec(context.Background(), usersTable)
+    if err != nil {
+        return err
+    }
+
+    _, err = conn.Exec(context.Background(), sharingTable)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// CreateNote inserts a new note into the Notes table.
+func CreateNote(conn *pgx.Conn, title string, content string, status string, userID int) error {
+    _, err := conn.Exec(context.Background(), "INSERT INTO Notes (title, content, timestamp, status, userID) VALUES ($1, $2, $3, $4, $5)",
+        title, content, time.Now(), status, userID)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// GetNoteByID retrieves a note by its ID.
+func GetNoteByID(conn *pgx.Conn, noteID int) (*Note, error) {
+    var note Note
+
+    err := conn.QueryRow(context.Background(), "SELECT * FROM Notes WHERE ID = $1", noteID).
+        Scan(&note.ID, &note.Title, &note.Content, &note.Timestamp, &note.Status, &note.UserID)
+    if err != nil {
+        return nil, err
+    }
+
+    return &note, nil
+}
+
+// UpdateNote updates an existing note.
+func UpdateNote(conn *pgx.Conn, noteID int, title string, content string, status string) error {
+    _, err := conn.Exec(context.Background(), "UPDATE Notes SET title = $1, content = $2, status = $3 WHERE ID = $4", title, content, status, noteID)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// DeleteNoteByID deletes a note by its ID.
+func DeleteNoteByID(conn *pgx.Conn, noteID int) error {
+    _, err := conn.Exec(context.Background(), "DELETE FROM Notes WHERE ID = $1", noteID)
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
--- main.go
+++ main.go
@@ -3,15 +3,52 @@ package main
 import (
 	"fmt"
 	"log"
+	"context"
 
 	"EnterpriseNotes/databasesetup"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	_, err := databasesetup.DatabaseSetup()
+	conn, err := databasesetup.DatabaseSetup()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	defer conn.Close(context.Background()) // Close the database connection when done
+
 	fmt.Println("Database setup completed successfully.")
+
+	// Example of creating a new note
+	err = databasesetup.CreateNote(conn, "Sample Note Title", "Sample Note Content", "Active", 1)
+	if err != nil {
+		log.Fatal("Failed to create a new note: ", err)
+	}
+
+	fmt.Println("Note created successfully.")
+
+	// retrieving a note by ID
+	note, err := databasesetup.GetNoteByID(conn, 1) 
+	if err != nil {
+		log.Fatal("Failed to retrieve a note: ", err)
+	}
+
+	fmt.Printf("Note ID: %d\nTitle: %s\nContent: %s\nStatus: %s\nUserID: %d\n",
+		note.ID, note.Title, note.Content, note.Status, note.UserID)
+
+	// updating a note
+	err = databasesetup.UpdateNote(conn, 1, "Updated Title", "Updated Content", "Inactive") 
+	if err != nil {
+		log.Fatal("Failed to update the note: ", err)
+	}
+
+	fmt.Println("Note updated successfully.")
+
+	// deleting a note by ID
+	err = databasesetup.DeleteNoteByID(conn, 1) 
+	if err != nil {
+		log.Fatal("Failed to delete the note: ", err)
+	}
+
+	fmt.Println("Note deleted successfully.")
 }
