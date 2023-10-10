77237a2fda41b387a1f4767c2edd946a0d62d9fc
Katherine M <102567666+KatherineMulder@users.noreply.github.com>
2023-10-10 18:55:26 +1300

    update HTML files and created a test file json

--- models/models.go
+++ models/models.go
@@ -5,31 +5,37 @@ import
 
 //Create Structs for the tables
 type Note struct {
-	NoteID         int    `db:"noteID"`
-	UserID         int    `db:"userID"`
-	NoteTitle      string `db:"noteTitle"`
-	NoteContent    string `db:"noteContent"`
-	CreationDate   time.Time `db:"creationDate"`
-	CompletionDate time.Time `db:"completionDate"`
-	Status         string `db:"status"`
+	NoteID         int    `json:"noteID"`
+	UserID         int    `json:"userID"`
+	NoteTitle      string `json:"noteTitle"`
+	NoteContent    string `json:"noteContent"`
+	CreationDate   time.Time `json:"creationDate"`
+	CompletionDate time.Time `json:"completionDate"`
+	Status         string `json:"status"`
 }
 
 type User struct {
-	UserID           int    `db:"userID"`
-	Username         string `db:"username"`
-	Password         string `db:"password"`
-	Email            string `db:"email"`
-	RegistrationDate string `db:"registrationDate"`
+	UserID           int    `json:"userID"`
+	Username         string `json:"username"`
+	Password         string `json:"password"`
+	Email            string `json:"email"`
+	RegistrationDate string `json:"registrationDate"`
 }
 
 type Sharing struct {
-	SharingID        int    `db:"sharingID"`
-	NoteID           int    `db:"noteID"`
-	UserID           int    `db:"userID"`
-	Timestamp        time.Time `db:"timestamp"`
-	WrittingSettings bool   `db:"writingSettings"`
+	SharingID        int    `json:"sharingID"`
+	NoteID           int    `json:"noteID"`
+	UserID           int    `json:"userID"`
+	Timestamp        time.Time `json:"timestamp"`
+	WrittingSettings bool   `json:"writingSettings"`
 }
 
+// Create Struct for Test Data
+type Data struct {
+	Users        []User        `json:"users"`
+	Notes        []Note        `json:"notes"`
+	Sharings     []Sharing     `json:"sharings"`
+}
 
 // Create Slices
 var Users []User
